d0a4b5de2dceb70641638957f3b71af1ef046f92
Giulio De Donato <liuggio@gmail.com>
2017-04-27 12:58:12 +0200

    better loop

--- script/loop-is-up.sh
+++ script/loop-is-up.sh
@@ -2,5 +2,5 @@
 
 while true;
 do
-    curl -sL -w "%{http_code}\n" http://ecs-demo-lb-1311122183.us-east-1.elb.amazonaws.com || echo "========FAILED=========";
+    curl -sL -w "%{http_code}\n" ${HTTP_LB-"http://ecs-demo-lb5-72464861.us-east-1.elb.amazonaws.com"} || echo "========FAILED=========";
 done
